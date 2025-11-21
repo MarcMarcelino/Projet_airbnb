0f398abe33b71598429252dad846956537c98ac6
Gil-Lael <ac.kouakou@ecole-ipssi.net>
2025-11-21 19:32:57 +0100

    ajout CSS et FORM

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connexion à la base de données MySQL avec PDO
-try {
-    $dbh = new 	PDO( 'mysql:host=localhost;dbname=football;charset=utf8', 'root', 'L@elwifi2025');
-}   catch (PDOException $e){
-        die($e->getMessage());
-}
